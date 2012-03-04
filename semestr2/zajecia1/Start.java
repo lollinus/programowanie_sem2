933763b4d6cae9980996934635921615f48e2faf
Karol Barski <karol@barski.eu>
2012-03-04 11:56:31 +0100

    Add print method overload

--- semestr2/zajecia1/Start.java
+++ semestr2/zajecia1/Start.java
@@ -2,11 +2,9 @@ package zajecia1;
 
 public class Start {
         public static void main(String [] args) {
-                Student student = new Student();
-                student.name = "Karol";
-                student.surname = "Barski";
-                student.birthYear = 78;
-
-                System.out.println("Student: " + student.name + " " + student.surname + " " + student.birthYear);
+                Student student = new Student("Karol", "Barski", 78);
+                student.print("**********************************************");
+                student.print();
+                student.print("**********************************************");
         }
 }
--- semestr2/zajecia1/Student.java
+++ semestr2/zajecia1/Student.java
@@ -5,7 +5,25 @@ package zajecia1;
  * Sample class storing Student information
  */
 public class Student {
-        public String name;     //< student name
-        public String surname;  //< student surname
-        public int birthYear; //< student birth name
+        public Student(String name, String surname, int birthYear) {
+                this.name = name;
+                this.surname = surname;
+                this.birthYear = birthYear;
+        }
+
+        public String format() {
+                return "Student { name: " + this.name + ", surname: " + this.surname + ", birthYear: " + this.birthYear + "}";
+        }
+
+        public void print() {
+                System.out.println(this.format());
+        }
+
+        public void print(String yo) {
+                System.out.println(yo);
+        }
+
+        private String name;      //< student name
+        private String surname;   //< student surname
+        private int    birthYear; //< student birth name
 }
